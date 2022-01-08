88469f25bc40857214eaf9fa95785ced95f9530f
Toby <watertoby@gmail.com>
2022-01-07 21:53:19 -0700

    u

---
+++ Python_data/seleium/pro1_1.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+PATH= Service("C:/Users/Toby/PycharmProjects/pythonProject/project1/chromedriver.exe")
+
+driver=webdriver.Chrome(service=PATH)
+
+driver.get("https://google.com")
+search= driver.find_element(By.NAME,"q")
+search.send_keys("比特幣")
+
+search.send_keys(Keys.RETURN)
+
+time.sleep(5)
+
+driver.quit()
